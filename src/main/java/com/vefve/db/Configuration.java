b49c4dd85511006a943664ec84345efdc1e02cb1
vefve <kubermehrotra@gmail.com>
2018-08-26 12:38:29 +0530

    Adding In-Memory B-Tree implementation.

--- src/main/java/com/vefve/db/Configuration.java
+++ src/main/java/com/vefve/db/Configuration.java
@@ -1,7 +1,11 @@
 package com.vefve.db;
 
 public class Configuration {
+	
 	public static final long MEMORY_STORAGE_SIZE = 10;
+	
 	public static final boolean USE_PERSISTENT_STORAGE = true;
+	
 	public static final float LOAD_FACTOR_THRESHOLD = 0.70f;
+	
 }
