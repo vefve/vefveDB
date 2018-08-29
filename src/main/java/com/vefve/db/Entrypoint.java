00fec0ab53396a3ac04b66ee3c8890d509ab1ee3
vefve <kubermehrotra@gmail.com>
2018-08-29 17:27:44 +0530

    Cleaning up.

--- src/main/java/com/vefve/db/Entrypoint.java
+++ src/main/java/com/vefve/db/Entrypoint.java
@@ -21,38 +21,38 @@ public class Entrypoint {
 	 */
 	public static void main(String[] args) throws CreateNodeException {
 		
-		VefveDB<String, String> st = new VefveDB<String, String>();
+		VefveDB<String, String> vefveDB = new VefveDB<String, String>();
 
-		st.put("www.cs.princeton.edu", "128.112.136.12");
-		st.put("www.cs.princeton.edu", "128.112.136.11");
-		st.put("www.princeton.edu", "128.112.128.15");
-		st.put("www.yale.edu", "130.132.143.21");
-		st.put("www.simpsons.com", "209.052.165.60");
-		st.put("www.apple.com", "17.112.152.32");
-		st.put("www.amazon.com", "207.171.182.16");
-		st.put("www.ebay.com", "66.135.192.87");
-		st.put("www.cnn.com", "64.236.16.20");
-		st.put("www.google.com", "216.239.41.99");
-		st.put("www.nytimes.com", "199.239.136.200");
-		st.put("www.microsoft.com", "207.126.99.140");
-		st.put("www.dell.com", "143.166.224.230");
-		st.put("www.slashdot.org", "66.35.250.151");
-		st.put("www.espn.com", "199.181.135.201");
-		st.put("www.weather.com", "63.111.66.11");
-		st.put("www.yahoo.com", "216.109.118.65");
-		st.put("www.apple.com", null);
+		vefveDB.put("www.cs.princeton.edu", "128.112.136.12");
+		vefveDB.put("www.cs.princeton.edu", "128.112.136.11");
+		vefveDB.put("www.princeton.edu", "128.112.128.15");
+		vefveDB.put("www.yale.edu", "130.132.143.21");
+		vefveDB.put("www.simpsons.com", "209.052.165.60");
+		vefveDB.put("www.apple.com", "17.112.152.32");
+		vefveDB.put("www.amazon.com", "207.171.182.16");
+		vefveDB.put("www.ebay.com", "66.135.192.87");
+		vefveDB.put("www.cnn.com", "64.236.16.20");
+		vefveDB.put("www.google.com", "216.239.41.99");
+		vefveDB.put("www.nytimes.com", "199.239.136.200");
+		vefveDB.put("www.microsoft.com", "207.126.99.140");
+		vefveDB.put("www.dell.com", "143.166.224.230");
+		vefveDB.put("www.slashdot.org", "66.35.250.151");
+		vefveDB.put("www.espn.com", "199.181.135.201");
+		vefveDB.put("www.weather.com", "63.111.66.11");
+		vefveDB.put("www.yahoo.com", "216.109.118.65");
+		vefveDB.put("www.apple.com", null);
 
-		System.out.println("cs.princeton.edu:  " + st.get("www.cs.princeton.edu"));
-		System.out.println("gmail.com:         " + st.get("www.gmail.com"));
-		System.out.println("simpsons.com:      " + st.get("www.simpsons.com"));
-		System.out.println("apple.com:         " + st.get("www.apple.com"));
-		System.out.println("ebay.com:          " + st.get("www.ebay.com"));
-		System.out.println("dell.com:          " + st.get("www.dell.com"));
+		System.out.println("cs.princeton.edu:  " + vefveDB.get("www.cs.princeton.edu"));
+		System.out.println("gmail.com:         " + vefveDB.get("www.gmail.com"));
+		System.out.println("simpsons.com:      " + vefveDB.get("www.simpsons.com"));
+		System.out.println("apple.com:         " + vefveDB.get("www.apple.com"));
+		System.out.println("ebay.com:          " + vefveDB.get("www.ebay.com"));
+		System.out.println("dell.com:          " + vefveDB.get("www.dell.com"));
 		System.out.println();
 
-//		System.out.println("size:    " + st.size());
-//		System.out.println("height:  " + st.height());
-		System.out.println(st);
+//		System.out.println("size:    " + vefveDB.size());
+//		System.out.println("height:  " + vefveDB.height());
+		System.out.println(vefveDB);
 		System.out.println();
 	}
 
